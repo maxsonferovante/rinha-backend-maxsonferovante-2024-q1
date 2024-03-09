1843f2aedcbd254df9e76d83370e17eab5520880
maxsonferovante <maxson@devnology.com.br>
2024-03-09 19:39:32 -0300

    Adiciona modelo de transação e cliente com métodos de depósito e saque

---
+++ app/api/infra/model.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel, Field, model_serializer
+
+
+class TransactionType(str, Enum):
+    credit = "c"
+    debit = "d"
+
+
+class Transaction(BaseModel):
+    amount: int = Field(alias="valor", gt=0)
+    type: TransactionType = Field(alias="tipo")
+    description: str = Field(alias="descricao")
+    created_at: datetime = Field(alias="realizado_em", default_factory=datetime.utcnow)
+
+class Client(BaseModel):
+    id: int = Field(alias="cliente_id", gt=0)
+    accont_limit: int = Field(alias="limite", ge=0)
+    accont_balance: int = Field(alias="saldo", ge=0)
+    transactions: list[Transaction] = Field(alias="ultimas_transacoes", default=[])
+
+    def deposit(self, transaction: Transaction):
+        self.balance += transaction.amount
+        self.transactions.append(transaction)
+
+    def withdraw(self, transaction: Transaction):
+        self.balance -= transaction.amount
+        self.transactions.append(transaction)
+    
+    @model_serializer
+    def serialize_model(self):
+        return {
+            "saldo": {
+                "total": self.account_balance,
+                "data_extrato": datetime.utcnow().isoformat(),
+                "limite": abs(self.account_limit),
+            },
+            "ultimas_transacoes": [
+                {
+                    "valor": t.amount,
+                    "tipo": t.type.value,
+                    "descricao": t.description,
+                    "realizada_em": t.created_at,
+                }
+                for t in self.transactions
+            ],
+        }
+    
+class Error (BaseModel):
+    code: int
+    message: str
