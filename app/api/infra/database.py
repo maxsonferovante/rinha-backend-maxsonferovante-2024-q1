1193e0e046bc08dc76d1ab6b0ee75d51504df7fc
maxsonferovante <maxsonferovante@gmail.com>
2024-03-10 01:20:52 -0300

    Adiciona classe Database com padrão Singleton

--- app/api/infra/database.py
+++ app/api/infra/database.py
@@ -1,11 +1,13 @@
 from psycopg_pool import ConnectionPool
-
 from api.settings import settings
+from api.singleton.singleton import SingletonMeta
 
-
-pool = ConnectionPool(
-    settings.database_url,
-    max_size= settings.database_pool_max_size,
-    min_size= settings.database_pool_min_size,
-    open= True
-)
+class Database(metaclass = SingletonMeta):
+    def __init__(self) -> None:
+        self.pool = ConnectionPool(
+            settings.database_url,
+            max_size= settings.database_pool_max_size,
+            min_size= settings.database_pool_min_size,
+            open= True
+        )
+    
